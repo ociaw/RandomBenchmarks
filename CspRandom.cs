76184f7ee663eb69fd1603f76b9098467096bf09
Michael Johnson <michael@ociaw.com>
2020-05-17 15:59:11 -0500

    Add project files.

---
+++ CspRandom.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SystemRandomBenchmark
+{
+    /// <summary>
+    /// Wraps <see cref="RNGCryptoServiceProvider"/>.
+    /// </summary>
+    public sealed class CspRandom : Random
+    {
+        private const Int32 BUFFER_LENGTH = 128;
+
+        private readonly RNGCryptoServiceProvider _rng;
+        private readonly UInt32[] _buffer;
+        private Int32 _index;
+
+        public CspRandom(RNGCryptoServiceProvider rng)
+        {
+            _rng = rng;
+            _buffer = new UInt32[BUFFER_LENGTH];
+            _index = BUFFER_LENGTH;
+        }
+
+        public static CspRandom Create() => new CspRandom(new RNGCryptoServiceProvider());
+
+        public UInt32 NextUInt32()
+        {
+            if (_index >= BUFFER_LENGTH)
+                Regen();
+
+            UInt32 value = _buffer[_index];
+            _index += 1;
+            return value;
+        }
+
+        public override Int32 Next() => Next(0, Int32.MaxValue);
+
+        public override Int32 Next(Int32 maxValue)
+        {
+            if (maxValue < 0)
+                throw new ArgumentOutOfRangeException(nameof(maxValue), maxValue, $"{nameof(maxValue)} must be positive.");
+
+            return Next(0, maxValue);
+        }
+
+        public override Int32 Next(Int32 minValue, Int32 maxValue)
+        {
+            if (minValue >= maxValue)
+                throw new ArgumentOutOfRangeException(nameof(maxValue), maxValue, $"{nameof(maxValue)} ({maxValue}) must be higher than or equal to {nameof(minValue)} ({minValue}).");
+
+            return NextInclusive(minValue, maxValue - 1);
+        }
+
+        public override void NextBytes(Span<Byte> buffer) => _rng.GetBytes(buffer);
+
+        public override void NextBytes(Byte[] buffer) => _rng.GetBytes(buffer);
+
+        public void Dispose() => _rng.Dispose();
+
+        /// <summary>
+        /// Fills the buffer with new random bytes and sets <see cref="_index"/> to 0.
+        /// </summary>
+        private void Regen()
+        {
+            Span<Byte> buffer = System.Runtime.InteropServices.MemoryMarshal.AsBytes<UInt32>(_buffer);
+            NextBytes(buffer);
+            _index = 0;
+        }
+
+        private Int32 NextInclusive(Int32 minValue, Int32 maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentOutOfRangeException(nameof(maxValue), maxValue, $"{nameof(maxValue)} ({maxValue}) must be higher than or equal to {nameof(minValue)} ({minValue}).");
+
+            var range = unchecked((UInt32)(maxValue - minValue + 1));
+            var intsToReject = range == 0 ? 0 : (UInt32.MaxValue - range + 1) % range;
+            var zone = UInt32.MaxValue - intsToReject;
+
+            while (true)
+            {
+                var unsigned = NextUInt32();
+                if (unsigned > zone)
+                    continue;
+
+                return unchecked((Int32)(unsigned % range) + minValue);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace SystemRandomBenchmark
+{
+    class Program
+    {
+        static void Main() => BenchmarkRunner.Run<RandomVsCsp>();
+    }
+
+    public class RandomVsCsp
+    {
+        private const Int32 BUFFER_LENGTH = 0x2000;
+        private const Int32 SAMPLE_COUNT = 0x1000;
+
+        private readonly Random _random = new Random(10);
+        private readonly CspRandom _cspRandom = CspRandom.Create();
+        private readonly Byte[] _buffer = new Byte[BUFFER_LENGTH];
+
+        [Benchmark]
+        public void RandomBytes() => _random.NextBytes(_buffer);
+
+        [Benchmark]
+        public void CspRandomBytes() => _cspRandom.NextBytes(_buffer);
+
+        [Benchmark]
+        public void RandomInt32()
+        {
+            for (Int32 i = 0; i < SAMPLE_COUNT; i++)
+                _random.Next();
+        }
+
+        [Benchmark]
+        public void CspRandomInt32()
+        {
+            for (Int32 i = 0; i < SAMPLE_COUNT; i++)
+                _cspRandom.Next();
+        }
+
+        [Benchmark]
+        public void RandomFullInt32()
+        {
+            for (Int32 i = 0; i < SAMPLE_COUNT; i++)
+                _random.Next(Int32.MinValue, Int32.MaxValue);
+        }
+
+        [Benchmark]
+        public void CspRandomFullInt32()
+        {
+            for (Int32 i = 0; i < SAMPLE_COUNT; i++)
+                _cspRandom.Next(Int32.MinValue, Int32.MaxValue);
+        }
+    }
+}
